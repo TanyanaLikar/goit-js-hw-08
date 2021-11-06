4ed7c4f98e63c18136da517340c20cf12d6b3428
TatyanaLikar <85054268+TanyanaLikar@users.noreply.github.com>
2021-11-06 12:16:52 +0200

    save

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+const formData = {};
+
+const refs ={
+    form: document.querySelector('.feedback-form'),
+    textarea: document.querySelector('textarea'),
+    submit: document.querySelector('button'),
+    email: document.querySelector('input')
+}
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.textarea.addEventListener('input', throttle(onTextareaInput, 500));
+refs.form.addEventListener('input', event =>{    
+    formData[event.target.name] = event.target.value   
+})
+
+populateTextarea();
+
+function onFormSubmit(event){
+    event.preventDefault();
+    event.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY)
+    // console.log('отправляем форму')
+}
+
+function onTextareaInput(event){
+ const message = event.target.value;
+ localStorage.setItem(STORAGE_KEY, message)
+//  console.log(message)
+}
+function populateTextarea(){
+    const saveMessage =localStorage.getItem(STORAGE_KEY);
+    if(saveMessage){
+        refs.textarea.value = saveMessage ;
+    }
+}
