6e073a512ed0ac9496d57187936ad8c505c2d992
TatyanaLikar <85054268+TanyanaLikar@users.noreply.github.com>
2021-11-06 10:04:12 +0200

    save 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -4,3 +4,21 @@ import { galleryItems } from './gallery-items';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+
+
+const cardsMarkup = createGalleryMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend',cardsMarkup);
+
+function createGalleryMarkup(galleryItems){
+   return galleryItems
+   .map(({preview, original,description}) => {
+        return `
+        <div>
+        <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a></div>`;
+    }).join(''); 
+    
+};
+const lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250, captionPosition: 'bottom'});
